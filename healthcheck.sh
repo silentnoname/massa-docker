441675fd6f4dd8d80fa86f9c61b97ef23239d2e3
silentnoname <635397523pl@gamil.com>
2022-12-14 21:17:25 +0800

    add more features

--- healthcheck.sh
+++ healthcheck.sh
@@ -15,15 +15,49 @@ WaitBootstrap() {
         fi
 }
 
-CheckNodeHealth() {
-        # Check node status and logs events
-    # If node is still bootstrapping, ruturn 0
-    Bootstrapped=$(WaitBootstrap)
-    if [ $Bootstrapped  -eq 1 ]
-        then
-                echo "[$(date +"%Y-%m-%d %H:%M:%S" --utc -d "+8 hours" )] Node is still bootstrapping" >>$PATH_HOME/healthcheck.txt
-                exit 0
+Backupwallet() {
+        # Backup wallet
+        cd $PATH_CLIENT
+        if [ ! -f $PATH_HOME/wallet.dat ]; then
+            cp wallet.dat $PATH_HOME/wallet.dat
         fi
+}
+
+CreateWalletAndBackup() {
+	## Create a wallet, stake and backup
+	# If wallet don't exist
+	cd $PATH_CLIENT
+	checkWallet=`massa-client -p $PASSWORD wallet_info | grep -c "Secret key"`
+	if ([ ! -e $PATH_CLIENT/wallet.dat ] || [ $checkWallet -lt 1 ])
+	then
+		# Generate wallet
+		cd $PATH_CLIENT
+		massa-client -p $PASSWORD wallet_generate_secret_key > /dev/null
+                sleep 10
+		if [ ! -f $PATH_HOME/wallet.dat ]; then
+                        cp wallet.dat $PATH_HOME/wallet.dat
+                        screen -dmS massa-client bash -c 'massa-client -p '$PASSWORD''
+                        echo "[$(date +"%Y-%m-%d %H:%M:%S" --utc -d "+8 hours" )]Backup wallet.dat to $PATH_HOME" >>$PATH_HOME/healthcheck.txt
+                fi
+	fi
+	# If staking_keys don't exist
+        cd $PATH_CLIENT
+	checkStackingKey=`massa-client -p $PASSWORD node_get_staking_addresses | grep -c -E "[0-z]{51}"`
+	if ([ ! -e $PATH_NODE/config/staking_wallet.dat ] || [ $checkStackingKey -lt 1 ])
+	then
+		# Get private key
+		cd $PATH_CLIENT
+		privKey=$(massa-client -p $PASSWORD wallet_info | grep "Secret key" | cut -d " " -f 3)
+		# Stake wallet
+		massa-client -p $PASSWORD node_add_staking_secret_keys $privKey > /dev/null
+		# Backup staking_wallet.dat 
+                if [ ! -e $PATH_HOME/staking_wallet.dat ]
+                then
+		        cp $PATH_NODE/config/staking_wallet.dat $PATH_HOME/staking_wallet.dat
+		        echo "[$(date +"%Y-%m-%d %H:%M:%S" --utc -d "+8 hours" )]Backup staking_wallet.dat to $PATH_HOME" >>$PATH_HOME/healthcheck.txt
+                fi
+	fi
+
         #backup  node_privkey.key
         if [ ! -e $PATH_HOME/node_privkey.key ]
 	then
@@ -31,6 +65,17 @@ CheckNodeHealth() {
 		cp $PATH_NODE/config/node_privkey.key $PATH_HOME/node_privkey.key
 		echo "[$(date +"%Y-%m-%d %H:%M:%S" --utc -d "+8 hours" )]Backup node_privkey.key to $PATH_HOME" >>$PATH_HOME/healthcheck.txt
 	fi
+}
+CheckNodeHealth() {
+        # Check node status and logs events
+        # If node is still bootstrapping, ruturn 0
+        Bootstrapped=$(WaitBootstrap)
+        if [ $Bootstrapped  -eq 1 ]
+        then
+                echo "[$(date +"%Y-%m-%d %H:%M:%S" --utc -d "+8 hours" )] Node is still bootstrapping" >>$PATH_HOME/healthcheck.txt
+                exit 0
+        fi
+        CreateWalletAndBackup
         cd $PATH_CLIENT
         checkGetStatus=$(timeout 2 massa-client -p $PASSWORD get_status | wc -l)
 
